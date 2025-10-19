9d7b6bb05fa35056fae5b38f5d049267b1a7c9f0
chankaran2996 <chandrasekaran2996@gmail.com>
2025-10-20 00:11:20 +0530

    motion added to my logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,30 @@
-
+import { motion } from "framer-motion";
 const Header = () => {
   return (
     <header className=" absolute w-full z-50 transition-all duration-300">
-        <div className=" container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between">
+        <div className=" container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 md:h-20">
             {/* logo */}
-            <div className=" flex items-center">
-                <div className="h-10 w-10 rounded-xl ">
+            <motion.div 
+            initial={{ x: -50, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ 
+                type: 'spring', 
+                stiffness: 100, 
+                damping: 25,
+                delay: 0.3,
+                duration: 1.2
+
+            }}
+            className=" flex items-center">
+                <div className="h-10 w-10 rounded-xl bg-gradient-to-r from-gray-500 to-gray-100
+                flex items-center justify-center text-purple-600 font-bold text-xl mr-3">
                     C
                 </div>
-            </div>
+                <span className=" font-bold text-xl bg-gradient-to-r from-gray-500 to-gray-100 
+                bg-clip-text text-transparent">
+                    Chandrasekaran
+                </span>
+            </motion.div>
         </div>
 
     </header>
