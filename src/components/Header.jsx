286d4d388f820828853a8d4b08a04d42fc581f8a
chankaran2996 <chandrasekaran2996@gmail.com>
2025-10-19 23:49:47 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+const Header = () => {
+  return (
+    <header className=" absolute w-full z-50 transition-all duration-300">
+        <div className=" container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between">
+            {/* logo */}
+            <div className=" flex items-center">
+                <div className="h-10 w-10 rounded-xl ">
+                    C
+                </div>
+            </div>
+        </div>
+
+    </header>
+  );
+};
+
+export default Header;
