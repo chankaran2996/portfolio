02cc2080a65eff7b68774ae29ffd44a1893cbe20
chankaran2996 <chandrasekaran2996@gmail.com>
2025-10-20 13:39:00 +0530

    added social icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
 import { motion } from "framer-motion";
+import { FaWhatsapp } from "react-icons/fa";
+import { FiGithub, FiLinkedin } from "react-icons/fi";
+
 const Header = () => {
     const navItems = ["Home", "About", "Projects", "Skills", "Contact"];
   return (
@@ -53,8 +56,53 @@ const Header = () => {
 
             {/* social icons */}
             <div className=" md:flex hidden items-center space-x-4">
+                <motion.a 
+                initial={{ scale: 0.5, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{
+                    delay: 1.3,
+                    duration: 0.8
+                }}
+                className=" text-gray-700 dark:text-gray-300 hover:text-violet-600 
+                hover:dark:text-violet-300 transition-colors duration-300" 
+                href="https://github.com/chankaran2996" target="_blank">
+                    <FiGithub className=" w-5 h-5"/>
+                </motion.a>
 
+                <motion.a 
+                initial={{ scale: 0.5, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{
+                    delay: 1.3,
+                    duration: 0.8
+                }}
+                className=" text-gray-700 dark:text-gray-300 hover:text-violet-600 
+                hover:dark:text-violet-300 transition-colors duration-300" 
+                href="https://www.linkedin.com/in/chandrasekaran-e-a5b58016a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" 
+                target="_blank">
+                    <FiLinkedin className=" w-5 h-5"/>
+                </motion.a>
+
+                <motion.a 
+                initial={{ scale: 0.5, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{
+                    delay: 1.3,
+                    duration: 0.8
+                }}
+                className=" text-gray-700 dark:text-gray-300 hover:text-violet-600 
+                hover:dark:text-violet-300 transition-colors duration-300" 
+                href="https://wa.me/918220345388" target="_blank">
+                    <FaWhatsapp className=" w-5 h-5"/>
+                </motion.a>
             </div>
+
+            {/* hire me  */}
+            <button className=" ml-4 px-4 py-2 rounded-xl bg-gradient-to-r from-gray-400 to-gray-100 
+            text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700 hover:text-white
+            transition-all duration-500">
+                Hire Me
+            </button>
         </div>
 
     </header>
