d4bea6232373843a2443e6f3dc3b2159d0cabae5
chankaran2996 <chandrasekaran2996@gmail.com>
2025-10-20 18:51:32 +0530

    hire me button is created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -95,14 +95,26 @@ const Header = () => {
                 href="https://wa.me/918220345388" target="_blank">
                     <FaWhatsapp className=" w-5 h-5"/>
                 </motion.a>
+
+                {/* hire me  */}
+                <motion.button 
+                initial={{ scale: 0.8, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{
+                    delay: 1.6,
+                    duration: 0.8,
+                    type: 'spring',
+                    stiffness: 100,
+                    damping: 15
+                }}
+                className=" ml-4 px-4 py-2 rounded-xl bg-gradient-to-r from-gray-400 to-gray-100 
+                text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700 hover:text-white
+                transition-all duration-500">
+                    Hire Me
+                </motion.button>
             </div>
 
-            {/* hire me  */}
-            <button className=" ml-4 px-4 py-2 rounded-xl bg-gradient-to-r from-gray-400 to-gray-100 
-            text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700 hover:text-white
-            transition-all duration-500">
-                Hire Me
-            </button>
+            {/*  */}
         </div>
 
     </header>
