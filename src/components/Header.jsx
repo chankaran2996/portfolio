c47394e0b6819dfa73db076ad780272f0a990dc1
chankaran2996 <chandrasekaran2996@gmail.com>
2025-10-20 12:44:52 +0530

    added nav items with motion effect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 const Header = () => {
+    const navItems = ["Home", "About", "Projects", "Skills", "Contact"];
   return (
     <header className=" absolute w-full z-50 transition-all duration-300">
         <div className=" container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 md:h-20">
@@ -25,6 +26,35 @@ const Header = () => {
                     Chandrasekaran
                 </span>
             </motion.div>
+            {/* nav */}
+            <nav className=" lg:flex hidden space-x-8">
+                {
+                    navItems.map((item, index) => (
+                        <motion.a 
+                        initial={{ y: -20, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        transition={{
+                            type: 'spring',
+                            stiffness: 100,
+                            damping: 20,
+                            delay: 0.7 + index * 0.2,
+                        }}
+                        key={index}
+                        href={`#`}
+                        className=" relative text-gray-800 dark:text-gray-200 hover:violet-600 
+                        dark:hover:text-violet-400 transition-colors duration-300 font-medium group">
+                            {item}
+                            <span className=" absolute left-0 bottom-0 w-0 h-0.5 bg-violet-600 
+                            dark:bg-violet-400 transition-all duration-300 group-hover:w-full"></span>
+                        </motion.a>
+                    ))
+                }
+            </nav>
+
+            {/* social icons */}
+            <div className=" md:flex hidden items-center space-x-4">
+
+            </div>
         </div>
 
     </header>
