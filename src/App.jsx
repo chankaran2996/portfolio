ecf2d29166a9c834430f9d5f5ae6877baa62c39e
chankaran2996 <chandrasekaran2996@gmail.com>
2025-11-17 01:32:45 +0530

    started hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header"
+import HeroSection from "./components/HeroSection"
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header />
+      <HeroSection />
     </>
   )
 }
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section 
+    className="h-screen bg-gradient-to-b from-violet-900 to-black flex items-center 
+    justify-between flex-col-reverse xl:flex-row px-10 lg:px-24 relative overflow-hidden"
+    >
+        {/* Left Section */}
+        <div>
+            <h1></h1>
+        </div>
+
+    </section>
+  )
+}
+
+export default HeroSection
